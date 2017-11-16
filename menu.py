e154bef3af556ce22b7c8cdfd29d61758380a397
Uyen Le <tle004@gold.ac.uk>
2017-11-16 17:33:28 +0000

    Encryption now uses privatekey and Decryption now uses publickey

--- menu.py
+++ menu.py
@@ -21,15 +21,15 @@ def menu():
     # print(pow(2,4,20))
     #
     # print("-----")
-    # print(modular_exp(3,5,7))
-    # print(modular_exp(4,2,12))
-    # print(modular_exp(9,3,50))
-    # print(modular_exp(5,2,19))
-    # print(modular_exp(33,2,125))
-    # print(modular_exp(5,9,11))
-    # print(modular_exp(4,4,22))
-    # print(modular_exp(9,8,7))
-    # print(modular_exp(2,4,20))
+    # print(modExp(3,5,7))
+    # print(modExp(4,2,12))
+    # print(modExp(9,3,50))
+    # print(modExp(5,2,19))
+    # print(modExp(33,2,125))
+    # print(modExp(5,9,11))
+    # print(modExp(4,4,22))
+    # print(modExp(9,8,7))
+    # print(modExp(2,4,20))
 
 
     regexStr = re.compile("^[a-zA-Z0-9,.].*$")
@@ -56,23 +56,21 @@ def menu():
         sys.exit(1)
     else:
 
-        # START RSA ENCRYPTION
-
         print("\n################ RSA Encryption - ALICE ###############")
 
         # Randomly generate keys p and q between 0 and 100
-        alice_p = generateRandKey()
-        alice_q = generateRandKey()
-        print('p is:', alice_p)
-        print('q is:', alice_q)
+        key_p = generateRandKey()
+        key_q = generateRandKey()
+        print('p is:', key_p)
+        print('q is:', key_q)
 
         # Caluculate key n
-        alice_n = calculateN(alice_p, alice_q)
-        print("n is:", str(alice_p), "*", str(alice_q), "=", alice_n)
+        key_n = calculateN(key_p, key_q)
+        print("n is:", str(key_p), "*", str(key_q), "=", key_n)
 
         # Calculate phi(n)
-        phiN = totient(alice_p, alice_q)
-        print("phi(n) is: (", str(alice_p), "-1) * (", str(alice_q), "-1) =", phiN)
+        phiN = totient(key_p, key_q)
+        print("phi(n) is: (", str(key_p), "-1) * (", str(key_q), "-1) =", phiN)
 
         # Generate key e
         # by adding all coprime numbers to phi(n) to a list
@@ -83,52 +81,57 @@ def menu():
             if(isCoPrime([i, phiN])):
                 coPrimeList.append(i)
 
-        alice_e = coPrimeList[random.randint(coPrimeList[0], len(coPrimeList)-1)]
-        print("\ne is: ", alice_e)
+        key_e = coPrimeList[random.randint(coPrimeList[0], len(coPrimeList)-1)]
+        print("\ne is: ", key_e)
 
         # Verify d is coprime to phiN
-        if gcd(alice_e,phiN) == 1:
+        if gcd(key_e,phiN) == 1:
             print("Verified e is coprime!")
         else:
             print("ERROR: E is not coprime. Please try again.")
             sys.exit(1)
 
         # Generate key d using Extended Euclidean algorithm
-        _, alice_d, _ = egcd(alice_e, phiN)
+        _, key_d, _ = egcd(key_e, phiN)
 
         # ensure key d is positive
-        if alice_d < 0:
-            alice_d = alice_d % phiN
+        if key_d < 0:
+            key_d = key_d % phiN
 
-        print("\nd is: ", alice_d)
+        print("\nd is: ", key_d)
 
         # verify d is coprime to phiN
-        if gcd(alice_d,phiN) == 1:
+        if gcd(key_d,phiN) == 1:
             print("Verified d is coprime!")
         else:
             print("ERROR: D is not coprime. Please try again.")
             sys.exit(1)
 
-        # Alice's public and private keys
-        alice_pubKey = alice_e, alice_n
-        print ("\nAlice public key is: ", alice_pubKey)
-        alice_privKey = alice_d, alice_n
-        print ("Alice private key is: ", alice_privKey)
+        # Public and private keys
+        publicKey = key_e, key_n
+        print ("\nPublic key is: ", publicKey)
+        privateKey = key_d, key_n
+        print ("Private key is: ", privateKey)
+        print("\n(!) You can now publish your public key, however your private key must be kept private!")
 
-        ciphertext = [ pow(ord(c), alice_e, alice_n) for c in inputMessage ]
+        # RSA Encryption with Private key
+        ciphertext = [ pow(ord(c), key_d, key_n) for c in inputMessage ]
         print("\nEncryption completed. Your ciphertext is: ", ciphertext)
 
-        send = str(input("\nPress S then Enter to send your ciphertext to Bob..."))
+        # test = [ modExp(ord(c), key_d, key_n) for c in inputMessage ]
+        # print("\ntest ", test)
+
+
+        send = str(input("\nPress S then Enter to send your ciphertext to Bob: "))
         if not send == 's':
             print("\nYou must send your ciphertext to Bob to continue.\n")
             sys.exit(1)
         else:
 
-            # START RSA DECRYPTION
-
             print("\n################ RSA Decryption - BOB #################")
 
-            plaintext = [ chr(pow(c, alice_d, alice_n)) for c in ciphertext ]
+            # RSA Decryption with Public key
+            plaintext = [ chr(pow(c, key_e, key_n)) for c in ciphertext ]
             print("\nDecryption completed. Original plaintext is: ", plaintext)
 
             #Verify decrypted message matches original plaintext
